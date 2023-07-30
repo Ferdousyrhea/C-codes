9115d5bf55a3aa96372cf915deefc46d09159a0b
Rezwoana Ferdousy Rhea <128154797+Ferdousyrhea@users.noreply.github.com>
2023-07-30 22:27:22 +0600

    Add files via upload

---
+++ C programming/Git c3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <math.h>
+int main()
+{
+
+    printf("\n%f",ceil(3.3));
+    printf("\n%f",floor(3.3));
+    printf("\n%f",sqrt(81));
+    printf("\n%f",pow(3,3));
+    printf("\n%d",abs(-33));
+    printf("\n");
+    return 0;
+}
